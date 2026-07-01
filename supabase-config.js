52ba24ac12ddb60bca5b374f2d97456788345956
alexmorimoto22 <alex.morimoto22@gmail.com>
2026-07-01 11:41:53 -0300

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,7 +0,0 @@
-window.VITE_SUPABASE_URL = "";
-window.VITE_SUPABASE_ANON_KEY = "";
-
-window.VM_AGRO_SUPABASE_CONFIG = {
-  url: window.VITE_SUPABASE_URL,
-  anonKey: window.VITE_SUPABASE_ANON_KEY
-};
