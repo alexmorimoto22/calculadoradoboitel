c15b60159bc64599e4a69c9ef990437a4af1d993
alexmorimoto22 <alex.morimoto22@gmail.com>
2026-07-02 09:01:04 -0300

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+window.VITE_SUPABASE_URL = "";
+window.VITE_SUPABASE_ANON_KEY = "";
+
+window.VM_AGRO_SUPABASE_CONFIG = {
+  url: window.VITE_SUPABASE_URL,
+  anonKey: window.VITE_SUPABASE_ANON_KEY
+};
